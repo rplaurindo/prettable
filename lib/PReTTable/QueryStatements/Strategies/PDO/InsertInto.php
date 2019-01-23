68602ada968e2e55011931a0f9cb79fd9559cff5
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-01-23 01:03:54 -0200

    fixes InsertInto

--- lib/PReTTable/QueryStatements/Strategies/PDO/InsertInto.php
+++ lib/PReTTable/QueryStatements/Strategies/PDO/InsertInto.php
@@ -2,38 +2,38 @@
 
 namespace PReTTable\QueryStatements\Strategies\PDO;
 
-use 
+use
     PReTTable\Repository\RelationshipBuilding,
     PReTTable\QueryStatementStrategyInterface;
 
 class InsertInto implements QueryStatementStrategyInterface {
-    
+
     private $tableName;
-    
+
     function __construct($modelName) {
         RelationshipBuilding::checkIfModelIs($modelName, 'PReTTable\ModelInterface');
-        
+
         $this->tableName = RelationshipBuilding::resolveTableName($modelName);
     }
-    
+
     function getStatement(array $attributes) {
-        $insertIntoStatement = 
+        $insertIntoStatement =
             "$this->tableName (" . implode(", ", array_keys($attributes)) . ")";
-        
+
         $values = [];
-        foreach ($attributes as $columnName) {
+        foreach (array_keys($attributes) as $columnName) {
             array_push($values, ":$columnName");
         }
-        
+
         $valuesStatement = implode(', ', $values);
-        
+
         $statement = "
             INSERT INTO $insertIntoStatement
             VALUES ($valuesStatement)
         ";
-        
+
         return $statement;
-        
+
     }
-    
+
 }
