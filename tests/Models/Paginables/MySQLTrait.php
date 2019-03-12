922d69f8a3a74fbb6b29b0643bb8aba5d37b6c48
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-03-12 13:40:47 -0300

    comment

--- tests/Models/Paginables/MySQLTrait.php
+++ tests/Models/Paginables/MySQLTrait.php
@@ -6,6 +6,7 @@ use
     QueryStatements\Decorators\Select\Pagination
 ;
 
+// to mixin code (not to repeat the the same code)
 trait MySQLTrait {
 
     function getAll($limit = null, $pageNumber = 1) {
