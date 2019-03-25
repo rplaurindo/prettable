dcc50110e221e97e140c8aba7e32e15271500094
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-03-25 12:41:01 -0300

    adequates Update as InsertInto

--- lib/PReTTable/QueryStatements/WriteStrategies/Update.php
+++ lib/PReTTable/QueryStatements/WriteStrategies/Update.php
@@ -3,42 +3,44 @@
 namespace PReTTable\QueryStatements\WriteStrategies\DAO;
 
 use
-    PReTTable\QueryStatements
+    PReTTable\QueryStatements,
+    PReTTable\Helpers\SQL
 ;
 
 class Update implements QueryStatements\StrategyInterface {
-    
+
     private $tableName;
-    
+
     private $primaryKeyName;
-    
+
     private $primaryKeyValue;
-    
+
     function __construct($tableName, $primaryKeyName, $primaryKeyValue) {
         $this->tableName = $tableName;
         $this->primaryKeyName = $primaryKeyName;
         $this->primaryKeyValue = $primaryKeyValue;
     }
-    
+
     function getStatement(array $attributes) {
         $whereStatement = "$this->primaryKeyName = $this->primaryKeyValue";
-        
+        $values = SQL\ValueAdjuster::adjust(...array_values($attributes));
+
         $settings = [];
-        
-        foreach (array_keys($attributes) as $columnName => $value) {
+        foreach (array_keys($attributes) as $index => $columnName) {
+            $value = $values[$index];
             array_push($settings, "$columnName = $value");
         }
-        
+
         $settingsStatement = implode(', ', $settings);
-        
+
         $statement = "
             UPDATE $this->tableName
 
             SET $settingsStatement
 
             WHERE $whereStatement";
-        
+
         return $statement;
     }
-    
+
 }
