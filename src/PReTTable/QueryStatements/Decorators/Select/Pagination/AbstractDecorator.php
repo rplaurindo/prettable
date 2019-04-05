aaff0ee4225d31b89a938551034a73e9b2cd32d1
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-04-04 23:45:46 -0300

    adequations

---
+++ src/PReTTable/QueryStatements/Decorators/Select/Pagination/AbstractDecorator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PReTTable\QueryStatements\Decorators\Select\Pagination;
+
+use
+    PReTTable\QueryStatements,
+    PReTTable\QueryStatements\AbstractComponent
+;
+
+abstract class AbstractDecorator extends QueryStatements\AbstractDecorator {
+    
+    protected $limit;
+    
+    protected $pageNumber;
+
+    function __construct(AbstractComponent $component, $limit = null, $pageNumber = 1) {
+        parent::__construct($component);
+        
+        $this->limit = $limit;
+        
+        $this->pageNumber = $pageNumber;
+    }
+
+}
--- src/PReTTable/QueryStatements/Decorators/Select/Pagination/MySQL.php
+++ src/PReTTable/QueryStatements/Decorators/Select/Pagination/MySQL.php
@@ -4,23 +4,22 @@ namespace PReTTable\QueryStatements\Decorators\Select\Pagination;
 
 use
     PReTTable\Helpers\Pagination,
-    PReTTable\QueryStatements\AbstractDecorator,
     PReTTable\QueryStatements\AbstractComponent
 ;
 
 class MySQL extends AbstractDecorator {
 
     function __construct(AbstractComponent $component, $limit = null, $pageNumber = 1) {
-        parent::__construct($component);
+        parent::__construct($component, $limit, $pageNumber);
         
         $this->_statement = $this->resolveStatement($limit, $pageNumber);
     }
 
-    private function resolveStatement($limit = null, $pageNumber = 1) {
-        $offset = Pagination::calculatesOffset($limit, $pageNumber);
+    private function resolveStatement() {
+        $offset = Pagination::calculatesOffset($this->limit, $this->pageNumber);
 
-        if (isset($limit)) {
-            return "LIMIT $limit\n\n\tOFFSET $offset";
+        if (isset($this->limit)) {
+            return "LIMIT $this->limit\n\n\tOFFSET $offset";
         }
 
         return '';
