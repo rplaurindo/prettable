a22573188b7af5fb3bf84c323d6c6b187640f860
Rafael Pereira Laurindo <rplaurindo>
2019-09-05 16:22:38 -0300

    improvements

--- src/PreTTable/QueryStatements/Decorators/Select/WhereClauseStatement.php
+++ src/PreTTable/QueryStatements/Decorators/Select/WhereClauseStatement.php
@@ -6,51 +6,97 @@ use
     PreTTable\Helpers\SQL\ValueAdjuster
 ;
 
-class WhereClauseStatement extends AbstractWhereClauseStatement {
+class WhereClauseStatement {
 
-    function __construct($table = null) {
-        parent::__construct($table);
+    private $tableName;
+    
+    private $statement;
+    
+    private $options = [];
+    
+    function __construct($tableName = null) {
+        $this->tableName = $tableName;
+        $this->statement = '';
+        
+        $this->options = [
+            'comparisonOperator' => '=',
+            'logicalOperator' => 'AND'
+        ];
     }
-
-    function like($columnName, $value) {
+    
+    function addStatements(array $params, $options = []) {
         $clone = $this->getClone();
-
+        
+        foreach($params as $columnName => $value) {
+            $clone->addStatementTo($columnName, $value, $options);
+        }
+        
+        return $clone;
+    }
+    
+    function addStatement($statement, $options = []) {
+        if (array_key_exists('logicalOperator', $options)) {
+            $logicalOperator = $options['logicalOperator'];
+        } else {
+            $logicalOperator = $this->options['logicalOperator'];
+        }
+        
+        if (empty($this->statement)) {
+            $this->statement .= $statement;
+        } else {
+            $this->statement .= "\n\t$logicalOperator $statement";
+        }
+    }
+    
+    function like($columnName, $value, $options = []) {
+        $clone = $this->getClone();
+        
         $columnStatement = $columnName;
-
+        
         if (isset($clone->tableName)) {
             $columnStatement = "$clone->tableName.$columnName";
         }
-
+        
         $value = ValueAdjuster::adjust([$value])[0];
-
+        
         $statement = "($columnStatement LIKE $value)";
-        $clone->addStatement($statement);
-
+        $clone->addStatement($statement, $options);
+        
         return $clone;
     }
-
-    function between($columnName, $start, $end) {
+    
+    function between($columnName, $start, $end, $options = []) {
         $clone = $this->getClone();
-
+        
         $columnStatement = $columnName;
-
+        
         if (isset($clone->tableName)) {
             $columnStatement = "$clone->tableName.$columnName";
         }
-
+        
         $statement = "$columnStatement BETWEEN $start AND $end";
-        $clone->addStatement($statement);
-
+        $clone->addStatement($statement, $options);
+        
         return $clone;
     }
-
-    protected function addStatementTo($columnName, $value) {
+    
+    function getStatement() {
+        return $this->statement;
+    }
+    
+    private function addStatementTo($columnName, $value, $options = []) {
+        if (array_key_exists('comparisonOperator', $options)) {
+            $comparisonOperator = $options['comparisonOperator'];
+        } else {
+            $comparisonOperator = $this->options['comparisonOperator'];
+        }
+        
         $columnStatement = $columnName;
-
+        
         if (isset($this->tableName)) {
             $columnStatement = "$this->tableName.$columnName";
         }
-
+        
         if (gettype($value) == 'array') {
             if (count($value)) {
                 $value = ValueAdjuster::adjust($value);
@@ -59,14 +105,18 @@ class WhereClauseStatement extends AbstractWhereClauseStatement {
             }
         } else {
             $value = ValueAdjuster::adjust([$value])[0];
-            $statement = "($columnStatement $this->comparisonOperator $value)";
+            $statement = "($columnStatement $comparisonOperator $value)";
         }
-
+        
         if (isset($statement)) {
-            $this->addStatement($statement);
+            $this->addStatement($statement, $options);
         }
-
+        
         return $this;
+    }
+    
+    private function getClone() {
+        return clone $this;
     }
 
 }
