07a29bde913a4daeb9f5035d720d3739638a44e6
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-09-06 03:25:29 -0300

    improvements

---
+++ src/PreTTable/WhereClause/InvolvedTableNames.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PreTTable\WhereClause;
+
+use
+    ArrayObject
+;
+
+class InvolvedTableNames {
+    
+    private $map;
+    
+    function __construct() {
+        $this->map = new ArrayObject();
+    }
+    
+    function getTableNameOfColumnName($columnName2Search) {
+        foreach ($this->map->getArrayCopy() as $tableName => $columnNames) {
+            foreach ($columnNames as $columnName) {
+                if ($columnName === $columnName2Search) {
+                    return $tableName;
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    function adds($tableName, array $columnNames) {
+        $clone = $this->getClone();
+        
+        $clone->map->offsetSet($tableName, $columnNames);
+        
+        return $clone;
+    }
+    
+    private function getClone() {
+        return clone $this;
+    }
+    
+}
