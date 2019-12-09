9a78c334d25a7ce92f18118dce847be7640d5ad0
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-12-09 15:49:07 -0300

    improvements

--- src/PreTTable/Helpers/SQL/ValueAdjuster.php
+++ src/PreTTable/Helpers/SQL/ValueAdjuster.php
@@ -8,6 +8,7 @@ class ValueAdjuster {
         $adjusted = [];
 
         foreach ($values as $value) {
+            $value = preg_replace("/'/", "\\'", $value);
             if (gettype($value) == 'string') {
                 array_push($adjusted, "'$value'");
             } else {
